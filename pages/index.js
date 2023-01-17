6ca2dfe8b89b3d6e955e244d80de553879036fee
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-17 09:49:50 +0300

    Used get static props to fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import Head from "next/head";
 
-export default function Home() {
+export default function Home({ articles }) {
+  console.log(articles);
   return (
     <div>
       <Head>
         <title>Home</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Home</h1>
+      <h1>articles</h1>
+      {articles.map((article) => (
+        <h3 key={article.id}>{article.title}</h3>
+      ))}
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://jsonplaceholder.typicode.com/posts?_limit=6");
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
